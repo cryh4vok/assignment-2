7942a47c9070848fe34b7425175976c57814cf6e
Simon Pletersek <pletersek.si@gmail.com>
2023-03-08 12:32:11 +0100

    assignment2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'assignment-2';
+  username = '';
+
+  constructor() {}
+
+  checkUsername() {
+    if (this.username === '') {
+      return true;
+    }
+  }
+
+  onSetUsername() {
+    this.username = '';
+  }
+  ngOnInit() {}
 }
